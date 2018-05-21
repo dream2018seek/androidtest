0bb0cda56e62702a924dd3c2bccc01ffb969e0a2
dream2018seek <350757686@qq.com>
2018-05-21 18:43:42 +0800

    intent3

---
+++ ActionMode/app/src/main/java/com/example/admin/actionmode/Item.java
@@ -0,0 +1,27 @@
+package com.example.admin.actionmode;
+
+public class Item {
+    private String Name;
+    private int ImageId;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getImageId() {
+        return ImageId;
+    }
+
+    public void setImageId(int imageId) {
+        ImageId = imageId;
+    }
+
+    public Item(String name, int imageId) {
+        Name = name;
+        ImageId = imageId;
+    }
+}
---
+++ Intentwebview/app/src/main/java/com/example/admin/intentwebview/Web.java
@@ -0,0 +1,34 @@
+package com.example.admin.intentwebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Web extends AppCompatActivity {
+
+    private WebView webview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        String url = getIntent().getExtras().getString("url");
+        webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.getSettings().setJavaScriptEnabled(false);
+        webview.getSettings().setAppCacheEnabled(true);
+        webview.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        webview.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webview.loadUrl(url);
+
+    }
+}
