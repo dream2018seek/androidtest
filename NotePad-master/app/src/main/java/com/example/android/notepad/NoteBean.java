a53fdb9b50f7796af35195dd860bad79c4eee02b
dream2018seek <350757686@qq.com>
2018-05-29 20:45:33 +0800

    center

---
+++ NotePad-master/app/src/main/java/com/example/android/notepad/NoteBean.java
@@ -0,0 +1,31 @@
+package com.example.android.notepad;
+
+public class NoteBean {
+    private String title;
+    private long date;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+}
